cedaa87e1cd26df674a78fac9d59de27ce0f18d2
raxhavijaya <raxhavijaya31@gmail.com>
2023-03-06 16:35:50 +0700

    first commit

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,24 @@
+import User from "../mongodb/models/user.js";
+
+const getAllUsers = async (req, res) => {};
+const createUser = async (req, res) => {
+  try {
+    const { name, email, avatar } = req.body;
+
+    const userExist = await User.findOne({ email });
+
+    if (userExist) return res.status(200), json(userExist);
+
+    const newUser = await User.create({
+      name,
+      email,
+      avatar,
+    });
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const getUserInfoByID = async (req, res) => {};
+
+export { getAllUsers, createUser, getUserInfoByID };
