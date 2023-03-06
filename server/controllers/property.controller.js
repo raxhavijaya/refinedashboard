1cf26b340e55d900720965c1f0e5a05859c297b8
raxhavijaya <raxhavijaya31@gmail.com>
2023-03-06 20:53:58 +0700

    project done

--- server/controllers/property.controller.js
+++ server/controllers/property.controller.js
@@ -86,7 +86,51 @@ const createProperty = async (req, res) => {
   }
 };
 
-const updateProperty = async (req, res) => {};
-const deleteProperty = async (req, res) => {};
+const updateProperty = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, propertyType, price, location, photo } = req.body;
+
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    await Property.findByIdAndUpdate(
+      { _id: id },
+      {
+        title,
+        description,
+        propertyType,
+        location,
+        price,
+        photo: photoUrl.url || photo,
+      }
+    );
+
+    res.status(200).json({ message: "Property updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteProperty = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const propertyToDelete = await Property.findById({ _id: id }).populate("creator");
+
+    if (!propertyToDelete) throw new Error("Property not found");
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    await Property.deleteOne({ _id: id }).session(session);
+    propertyToDelete.creator.allProperties.pull(propertyToDelete);
+
+    await propertyToDelete.creator.save({ session });
+    await session.commitTransaction();
+    res.status(200).json({ message: "Property deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export { getAllProperties, getPropertyDetail, createProperty, updateProperty, deleteProperty };
